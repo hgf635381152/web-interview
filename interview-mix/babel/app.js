23534aff20a6fadc21adc96231f97e30b9415749
hgf635381152 <635381152@qq.com>
2020-08-15 15:06:21 +0800

    babel

---
+++ interview-mix/babel/app.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+
+const sum = 2 ** 2
+class App extends Component {
+  
+  render() {
+    return (
+      <div>
+        <img src="baidu.com" alt="baidu" className="box" />
+      </div>
+    );
+  }
+}
+
+export default App;
