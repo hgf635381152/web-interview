37cc026ad6e49141bc572f573bc86cc32117d857
hgf635381152 <635381152@qq.com>
2020-08-17 14:49:07 +0800

    babel

--- interview-mix/babel/src/App.js
+++ interview-mix/babel/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import styles from './style.css'
 
 export default function App() {
   return (
     <div>
-      this is app
+      <div 
+        // className="swiper-container"
+        className={styles.swiper-container}
+      >
+        <img src="../public/ip6x2.png" alt=""></img>
+        this is app
+      </div>
     </div>
   )
 }
--- interview-mix/babel/webpack.config.js
+++ interview-mix/babel/webpack.config.js
@@ -1,3 +1,6 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const CopyPlugin = require('copy-webpack-plugin');
 
 const config = {
   entry: './src/index.js',
@@ -5,7 +8,7 @@ const config = {
     extensions: ['.js', '.jsx']
   },
   devServer: {
-    contentBase: './dist',
+    contentBase: './public',
     hot: true
   },
   module: {
@@ -14,8 +17,40 @@ const config = {
         test: /(.js|.jsx)$/,
         use: ['babel-loader'],
         exclude: /node_modules/
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                mode: 'local',
+                localIdentName: '[hash:base64:5'
+              }
+            }
+          }
+        ],
+        // css-loader css 模块
+        // style-loader  css内容 插到html的 style 标签内
+        // css module 解决css命名冲突
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html'
+    }),
+    new BundleAnalyzerPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { from: './public/ip6x2.png', to: './' },
+      ],
+    }),
+  ]
 }
 module.exports = config
+
+// dist 存着 项目打包的最终结果
+// dist 也是最终上线的文件夹
